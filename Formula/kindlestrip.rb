b0ea72ffe34d12a750b92790a12671dd4cbf46d8
Michael Maltese <mike@mikemaltese.com>
2013-04-02 00:25:38 +0000

    Update kindlestrip to 1.35-dilo_sec

--- Formula/kindlestrip.rb
+++ Formula/kindlestrip.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Kindlestrip < Formula
   homepage 'http://www.mobileread.com/forums/showthread.php?t=96903'
-  url 'http://www.mobileread.com/forums/attachment.php?attachmentid=94998&d=1351334468'
-  sha1 'df69be6ec539846cb647dd26193d5a79b8ed0b7a'
-  version '1.35'
+  url 'http://www.mobileread.com/forums/attachment.php?attachmentid=91128&d=1345662330'
+  sha1 'f08035836e86761c7e57028b443f07b27c7e87f6'
+  version '1.35-dilo_sec'
 
   def install
     mv "kindlestrip.py", "kindlestrip"
